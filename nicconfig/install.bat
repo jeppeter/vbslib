c57e47e5b2c986cf43c064d61e1453efb6620ace
jeppeter <jeppeter@gmail.com>
2017-08-27 14:19:19 +0800

    not copy btcmd

--- nicconfig/install.bat
+++ nicconfig/install.bat
@@ -11,7 +11,6 @@ copy /Y %FROMDIR%\baseop.vbs %INSTDIR%\baseop.vbs
 copy /Y %FROMDIR%\cmdexec.vbs %INSTDIR%\cmdexec.vbs
 copy /Y %FROMDIR%\vbsjson.vbs %INSTDIR%\vbsjson.vbs
 copy /Y %FROMDIR%\changeip.vbs %INSTDIR%\changeip.vbs
-copy /Y %FROMDIR%\btcmd %WINDIR%\..\btcmd
 
 
 schtasks.exe /Create /TN btbootcmd /TR "\"%WINDIR%\system32\cscript.exe\" \"%INSTDIR%\changeip.vbs\"" /RU system /SC ONSTART
