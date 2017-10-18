f31ccca92cf4dea1d6c8dedc5858678da9133f47
jeppeter <jeppeter@gmail.com>
2017-10-18 13:07:46 +0800

    to add install.bat with wevtutil enable true

--- nicconfig/install.bat
+++ nicconfig/install.bat
@@ -12,6 +12,6 @@ copy /Y %FROMDIR%\cmdexec.vbs %INSTDIR%\cmdexec.vbs
 copy /Y %FROMDIR%\vbsjson.vbs %INSTDIR%\vbsjson.vbs
 copy /Y %FROMDIR%\changeip.vbs %INSTDIR%\changeip.vbs
 
-
+wevtutil set-log Microsoft-Windows-TaskScheduler/Operational /enabled:true
 schtasks.exe /Create /TN btbootcmd /TR "\"%WINDIR%\system32\cscript.exe\" \"%INSTDIR%\changeip.vbs\"" /RU system /SC ONSTART
 echo on
